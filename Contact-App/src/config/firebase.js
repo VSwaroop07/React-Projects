10ef23a00d3b6c1aaf081092aa05913b1cde2d69
Swaroop Vyawahare <vyawahareswarup19@gmail.com>
2024-11-24 17:52:05 +0530

    Update firebase.js

--- Contact-App/src/config/firebase.js
+++ Contact-App/src/config/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAfd4fntTtPnesUljVJKAY2Xz87S6WYpeY",
-  authDomain: "contact-app-f8ea9.firebaseapp.com",
-  projectId: "contact-app-f8ea9",
-  storageBucket: "contact-app-f8ea9.firebasestorage.app",
-  messagingSenderId: "657707449428",
-  appId: "1:657707449428:web:2365310cd754f5727d81c1"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
